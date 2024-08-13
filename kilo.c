8543d1ae7f897b55833fd468a881e60046d64f09
Jinny Kim <yeajinny.k@gmail.com>
2024-08-13 16:41:11 -0600

    included a way to disable raw mode automatically

--- kilo.c
+++ kilo.c
@@ -1,11 +1,23 @@
+#include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
 
+// global variables
+struct termios orig_termios;
+
+
+// functions
+void disableRawMode() {
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
 
 void enableRawMode() {
-	struct termios raw;
 
-	tcgetattr(STDIN_FILENO, &raw); // read current attributes into a struct
+	tcgetattr(STDIN_FILENO, &orig_termios); // read current attributes into a struct
+	atexit(disableRawMode); // atexit causes disableRawMode() to be called automatically when program exits.
+
+	struct termios raw = orig_termios;
 
 	raw.c_lflag &= ~(ECHO);
 

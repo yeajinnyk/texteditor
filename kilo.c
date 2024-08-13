bf4560058e3082968d4749e29a91e366f9d2576d
Jinny Kim <yeajinny.k@gmail.com>
2024-08-13 16:36:08 -0600

    turning off echoing

--- kilo.c
+++ kilo.c
@@ -1,6 +1,23 @@
+#include <termios.h>
 #include <unistd.h>
 
+
+void enableRawMode() {
+	struct termios raw;
+
+	tcgetattr(STDIN_FILENO, &raw); // read current attributes into a struct
+
+	raw.c_lflag &= ~(ECHO);
+
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+	// TCSAFLUSH specifies when to apply the change
+
+}  
+
+
 int main() {
+
+	enableRawMode();
 
 	char c;
 	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');

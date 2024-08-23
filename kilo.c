6d5eb37d3c844fcf1331d1ee5064bedd728e2ff3
Jinny Kim <yeajinny.k@gmail.com>
2024-08-22 19:15:37 -0600

    welcome message

--- kilo.c
+++ kilo.c
@@ -14,6 +14,7 @@
 
 #define CTRL_KEY(k) ((k) & 0x1f)
 
+#define KILO_VERSION "0.0.1"
 
 /*** data ***/
 
@@ -138,7 +139,18 @@ void abFree(struct abuf *ab) {
 void editorDrawRows(struct abuf *ab) {
 	int y;
 	for (y = 0; y < E.screenrows; y++) {
-		abAppend(ab, "~", 1);
+		if (y == E.screenrows / 3) {
+			char welcome[80];
+			int welcomelen = snprintf(welcome, sizeof(welcome), "Kilo editor -- version %s", KILO_VERSION);
+			if (welcomelen > E.screencols) welcomelen = E.screencols;
+			abAppend(ab, welcome, welcomelen);
+		}
+		else {
+			abAppend(ab, "~", 1);
+		}
+
+
+		abAppend(ab, "\x1b[K", 3);
 
 		if (y < E.screenrows - 1) {
 			abAppend(ab, "\r\n", 2);
@@ -152,7 +164,7 @@ void editorRefreshScreen() {
 	struct abuf ab = ABUF_INIT;
 
 	abAppend(&ab, "\x1b[?25l", 6);
-	abAppend(&ab, "\x1b[2J", 4);		// the first byte  "\x1b" is the escape character (27 in decimal)
+	// the first byte  "\x1b" is the escape character (27 in decimal)
 	abAppend(&ab, "\x1b[H", 3);										// other 3 bytes is "[2J"
 							// escape sequence always starts with escape character (27) followed by [.
 							// J command (Erase In Display) to clear screen. 2 says clear entire screen

2b605bd224aed0a42b4e4cf32c16f4361893fc05
Jinny Kim <yeajinny.k@gmail.com>
2024-08-28 19:19:44 -0600

    allows scrolling

--- kilo.c
+++ kilo.c
@@ -47,6 +47,7 @@ typedef struct erow {		// "editor row". stores a line of text as a pointer to th
 struct editorConfig {
 	int cx, cy; 	// E.cx is the horizontal coord of the cursor (col) and E.cy is the vertical coord of the cursor (row)
 	int rowoff;	// keeps track of what row of the file the user is currently scrolled to
+	int coloff;
 	int screenrows;
 	int screencols;
 
@@ -251,10 +252,31 @@ void abFree(struct abuf *ab) {
 
 /*** output ***/
 
+
+void editorScroll() {
+	// vertical
+	if (E.cy < E.rowoff) {
+		E.rowoff = E.cy;
+	}
+	if (E.cy >= E.rowoff + E.screenrows) {
+		E.rowoff = E.cy - E.screenrows + 1;
+	}
+
+	// horizontal
+	if (E.cx < E.coloff) {
+		E.coloff = E.cx;
+	}
+	if (E.cx >= E.coloff + E.screencols) {
+		E.coloff = E.cx - E.screencols + 1;
+	}
+}
+
 void editorDrawRows(struct abuf *ab) {
 	int y;
 	for (y = 0; y < E.screenrows; y++) {
-		if (y >= E.numrows) {
+
+		int filerow = y + E.rowoff;
+		if (filerow >= E.numrows) {
 
 			// welcome message only displays if text buffer is empty
 			if (E.numrows == 0 && y == E.screenrows / 3) {
@@ -279,10 +301,10 @@ void editorDrawRows(struct abuf *ab) {
 
 		}
 		else {
-			int len = E.row[y].size;
-			
+			int len = E.row[filerow].size - E.coloff;
+			if (len < 0) len = 0;
 			if (len > E.screencols) len = E.screencols;
-			abAppend(ab, E.row[y].chars, len);
+			abAppend(ab, &E.row[filerow].chars[E.coloff], len);
 		}
 
 		abAppend(ab, "\x1b[K", 3);
@@ -295,6 +317,7 @@ void editorDrawRows(struct abuf *ab) {
 
 
 void editorRefreshScreen() {
+	editorScroll();
 
 	struct abuf ab = ABUF_INIT;
 
@@ -307,7 +330,7 @@ void editorRefreshScreen() {
 	editorDrawRows(&ab);
 
 	char buf[32];
-	snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1);
+	snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, (E.cx - E.coloff) +  1);
 	abAppend(&ab, buf, strlen(buf));
 
 	abAppend(&ab, "\x1b[?25h", 6); // hiding and showing cursor
@@ -323,6 +346,9 @@ void editorRefreshScreen() {
 
 // use WASD to move the cursor
 void editorMoveCursor(int key) {
+
+	erow *row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+
 	switch (key) {
 		case ARROW_LEFT:
 			if (E.cx != 0) {
@@ -330,7 +356,7 @@ void editorMoveCursor(int key) {
 			}
 			break;
 		case ARROW_RIGHT:
-			if (E.cx != E.screencols - 1) {
+			if (row && E.cx < row->size) {
 				E.cx++;
 			}
 			break;
@@ -340,10 +366,16 @@ void editorMoveCursor(int key) {
 			}
 			break;
 		case ARROW_DOWN:
-			if (E.cy != E.screenrows - 1) {
+			if (E.cy < E.numrows) {
 				E.cy++;
 			}
 			break;
+	}
+
+	row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+	int rowlen = row ? row->size : 0;
+	if (E.cx > rowlen) {
+		E.cx = rowlen;
 	}
 }
 
@@ -394,6 +426,7 @@ void initEditor() { 	// initializes all the fields in the E struct
 	E.cx = 0;
 	E.cy = 0;
 	E.rowoff = 0;
+	E.coloff = 0;
 	E.numrows = 0;
 	E.row = NULL;
 

aa90a81d5356c183fb935469110494b5432d9ef9
Jinny Kim <yeajinny.k@gmail.com>
2024-08-30 17:07:33 -0600

    implemented status message that helps user to quit if necessary

--- kilo.c
+++ kilo.c
@@ -7,6 +7,7 @@
 #include <ctype.h>
 #include <errno.h>
 #include <stdio.h>
+#include <stdarg.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/ioctl.h>
@@ -399,6 +400,16 @@ void editorDrawStatusBar(struct abuf *ab) {
 		}
 	}
 	abAppend(ab, "\x1b[m", 3);
+	abAppend(ab, "\r\n", 2);
+}
+
+void editorDrawMessageBar(struct abuf *ab) {
+	abAppend(ab, "\x1b[K", 3);
+	int msglen = strlen(E.statusmsg);
+	
+	if (msglen > E.screencols) msglen = E.screencols;
+	if (msglen && time(NULL) - E.statusmsg_time < 5) abAppend(ab, E.statusmsg, msglen);
+
 }
 
 
@@ -415,6 +426,7 @@ void editorRefreshScreen() {
 							// H command positions cursor
 	editorDrawRows(&ab);
 	editorDrawStatusBar(&ab);
+	editorDrawMessageBar(&ab);
 
 	char buf[32];
 	snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, (E.rx - E.coloff) +  1);
@@ -425,6 +437,15 @@ void editorRefreshScreen() {
 	write(STDOUT_FILENO, ab.b, ab.len);
 	abFree(&ab);
 
+}
+
+// takes in a format string and a variable number of arguments
+void editorSetStatusMessage(const char *fmt, ...) {
+	va_list ap;
+	va_start(ap, fmt);
+	vsnprintf(E.statusmsg, sizeof(E.statusmsg), fmt, ap);
+	va_end(ap);
+	E.statusmsg_time = time(NULL);
 }
 
 
@@ -541,7 +562,7 @@ void initEditor() { 	// initializes all the fields in the E struct
 
 	if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
 
-	E.screenrows -= 1;
+	E.screenrows -= 2;
 }
 
 int main(int argc, char *argv[]) {
@@ -552,6 +573,8 @@ int main(int argc, char *argv[]) {
 	if (argc >= 2) {
 		editorOpen(argv[1]);
 	}
+
+	editorSetStatusMessage("HELP: Ctrl-Q = quit");
 
 	while (1) {
 		editorRefreshScreen();
